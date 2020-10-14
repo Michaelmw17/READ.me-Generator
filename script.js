6d7ef5b0eae76b1c5bc3ab24912e4d0a8ac2d826
Michaelmw17 <Michaelmw17@outlook.com>
2020-10-14 17:39:39 +1100

    Attempted layout for read me and node script.js

---
+++ script.js
@@ -0,0 +1,101 @@
+// Declaring dependencies and variables
+const fs = require("fs");
+const util = require("util");
+const inquirer = require("inquirer");
+const generateReadMe = require("./utils/generateReadMe")
+const writeFileAsync = util.promisify(fs.writeFile);
+
+//Prompt user question's to populate the README.md
+// WHEN I am prompted for information about my application repository
+// THEN a quality, professional README.md is generated with the title of your project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+// WHEN I enter my project title
+// THEN this is displayed as the title of the README
+// WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
+// THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
+// WHEN I choose a license for my application from a list of options
+// THEN a badge for that license is added hear the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
+// WHEN I enter my GitHub username
+// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
+// WHEN I enter my email address
+// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
+function promptUser(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "projectTitle",
+            message: "Enter Project title? ",
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a brief description of your project: "
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Explain installation process if any: ",
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What is this project usage for? "
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Choose suitable license for this project: ",
+            choices: [
+                "Apache",
+                "GNU",
+                "ISC",
+                "MIT",
+                "Mozilla",
+                "Open"
+            ]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Whom are contributors of this project?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Is a test included? "
+        },
+        {
+            type: "input",
+            name: "questions",
+            message: "What needs to be done if I have an issue? "
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Please enter your email: "
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Please enter your GitHub username: "
+        }
+    ]);
+} 
+
+// Async function using util.promisify 
+  async function init() {
+    try {
+        // Asking user question's and generate responses
+        const answers = await promptUser();
+        const generateContent = generateReadMe(answers);
+        // Write new README.md to dist-directory
+        await writeFileAsync('./dist/README.md', generateContent);
+        console.log('Successful! wrote to README.md');
+    }   catch(err) {
+        console.log(err);
+    }
+  }
+  
+  init();  

627c24e45d7eb698e74c5f33d758a5af3dad1cec
Michaelmw17 <Michaelmw17@outlook.com>
2020-10-19 15:14:06 +1100

    Reacatoring for submission Requirement

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-const generateReadMe = require("./utils/generateReadMe")
+const generateMarkdown = require("./utils/generateMarkdown")
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
@@ -22,7 +22,7 @@ const writeFileAsync = util.promisify(fs.writeFile);
 // THEN I am taken to the corresponding section of the README
 
 //Prompt user question's to populate the README.md
-    promptUser = () => {
+    promptUserQuestions = () => {
     return inquirer.prompt([
         {
             type: "input",
@@ -32,17 +32,17 @@ const writeFileAsync = util.promisify(fs.writeFile);
         {
             type: "input",
             name: "description",
-            message: "Please Enter a brief description of your project: "
+            message: "Please enter a brief description of your project: "
         },
         {
             type: "input",
             name: "installation",
-            message: "Explain commands for installation process if any: ",
+            message: "Explain commands for installation dependencies? ",
         },
         {
             type: "input",
-            name: "usage",
-            message: "What commands should be run to run test "
+            name: "tests",
+            message: "Is a test included? "
         },
         {
             type: "list",
@@ -59,13 +59,13 @@ const writeFileAsync = util.promisify(fs.writeFile);
         },
         {
             type: "input",
-            name: "contributing",
-            message: "What does user need to know about contributing to this repository?"
+            name: "usage",
+            message: "What commands should be run to run test? "
         },
         {
             type: "input",
-            name: "tests",
-            message: "Is a test included? "
+            name: "contributing",
+            message: "What does user need to know about contributing to the repository?"
         },
         {
             type: "input",
@@ -86,16 +86,16 @@ const writeFileAsync = util.promisify(fs.writeFile);
 } 
 
 // Async function using util.promisify 
-  async function init() {
+  init = async () =>  {
     try {
         // Asking user question's and generate responses
-        const answers = await promptUser();
-        const generateContent = generateReadMe(answers);
+        const answers = await promptUserQuestions();
+        const generateContent = generateMarkdown(answers);
         // Write new README.md to dist-directory
         await writeFileAsync('./dist/README.md', generateContent);
         console.log('Successful! wrote to README.md');
     }   catch(err) {
-        console.log(err);
+        console.log('Error!');
     }
   }
   
